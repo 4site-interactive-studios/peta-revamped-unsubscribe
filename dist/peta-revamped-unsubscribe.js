d028aea7e2633b08b0e25a32d6b7912ff817c9a0
bryancasler <bryan.casler@gmail.com>
2023-07-19 18:13:20 -0400

    Uncheck the Master Suppression opt-in checkbox when landing on one of the cold pages

--- dist/peta-revamped-unsubscribe.js
+++ dist/peta-revamped-unsubscribe.js
@@ -1222,6 +1222,26 @@ const runScript = () => {
     });
   }
 
+     // If the URL is 53017, wich is for cold visitors, we should unchecked the Master Supression list checkbox on page load
+     /**
+     * This function unchecks the "supporter.questions.178" checkbox if it exists.
+     * It runs only when "53017" is present anywhere in the URL.
+     */
+    function uncheckQuestions178Checkbox() {
+      const url = window.location.href;
+      if (url.includes("53017")) {
+        const questions178Checkbox = document.querySelector("input[name='supporter.questions.178']");
+        if (questions178Checkbox) {
+          questions178Checkbox.checked = false;
+        }
+      }
+    }
+
+    // Call the function to uncheck the checkbox if the condition is met
+    uncheckQuestions178Checkbox();
+
+
+
   console.log("PETA Custom JS loaded");
   document.body.setAttribute("data-custom-js", "loaded");
 };
